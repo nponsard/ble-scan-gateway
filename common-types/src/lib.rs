ffc871fd5c0f715a33734ec7b4bf13ac5eebc816
Nils Ponsard <nils.ponsard@inria.fr>
2025-10-02 16:27:20 +0200

    feat: common types for communication over the network

--- common-types/src/lib.rs
+++ common-types/src/lib.rs
@@ -1,10 +1,11 @@
 #![no_std]
 
 use heapless::Vec;
+use serde::{Deserialize, Serialize};
 
 pub const MAX_SEEN: usize = 128;
 
-#[derive(Debug, serde::Serialize, serde::Deserialize, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct AddressesSeen {
     pub addrs: Vec<[u8; 6], MAX_SEEN>,
 }
@@ -16,4 +17,18 @@ impl From<Vec<bt_hci::param::BdAddr, MAX_SEEN>> for AddressesSeen {
             addrs: input.iter().map(|a| a.into_inner()).collect(),
         }
     }
+}
+
+#[derive(Clone, Copy, Debug, Serialize, Deserialize)]
+pub struct Location {
+    pub latitude: f32,
+    pub longitude: f32,
+    pub altitude: f32,
+    pub timestamp: u64,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct GatewayUpdate {
+    pub location: Option<Location>,
+    pub seen: Vec<[u8; 6], MAX_SEEN>,
 }
