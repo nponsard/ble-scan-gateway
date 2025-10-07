75232ae0b29feecb8a42a7bed139021d14712fdf
Nils Ponsard <nils.ponsard@inria.fr>
2025-10-07 13:50:47 +0200

    feat: improve server output formatting

--- server/src/main.rs
+++ server/src/main.rs
@@ -19,6 +19,7 @@ async fn main() {
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:4230").await.unwrap();
+    println!("Listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
 
@@ -41,11 +42,21 @@ async fn receive_mac(extract::Json(payload): extract::Json<GatewayUpdate>) -> (S
     println!("Altitude: {:?}", payload.location.map(|loc| loc.altitude));
     println!("Number of MAC addresses: {}", payload.seen.len());
 
-    for seen in &payload.seen {
-        for byte in seen {
-            print!("{:02X}:", byte);
+    // If there are too many addresses, print them all on one line
+    if payload.seen.len() > 40 {
+        println!(
+            "{}",
+            payload
+                .seen
+                .iter()
+                .map(|seen| seen.map(|s| s.to_string()).join(":"))
+                .collect::<Vec<String>>()
+                .join(", ")
+        );
+    } else {
+        for seen in &payload.seen {
+            println!("{}", seen.map(|s| s.to_string()).join(":"));
         }
-        println!();
     }
 
     (StatusCode::OK, "Ok".to_string())
