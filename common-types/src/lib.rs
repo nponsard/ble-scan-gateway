74651d73054f3bc6555da7470a3918788d18a534
Nils Ponsard <nils.ponsard@inria.fr>
2025-12-01 16:52:01 +0100

    feat: update types according to schema
    Signed-off-by: Nils Ponsard <nils.ponsard@inria.fr>

--- common-types/src/lib.rs
+++ common-types/src/lib.rs
@@ -6,23 +6,14 @@ use serde::{Deserialize, Serialize};
 #[cfg(feature = "std")]
 use std::vec::Vec;
 
-pub const MAX_SEEN: usize = 128;
+pub const MAX_SEEN: usize = 32;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct AddressesSeen {
     #[cfg(feature = "std")]
-    pub addrs: Vec<[u8; 6]>,
+    pub addrs: Vec<DetectedTag>,
     #[cfg(not(feature = "std"))]
-    pub addrs: Vec<[u8; 6], MAX_SEEN>,
-}
-
-#[cfg(feature = "from-impl")]
-impl From<Vec<bt_hci::param::BdAddr, MAX_SEEN>> for AddressesSeen {
-    fn from(input: Vec<bt_hci::param::BdAddr, MAX_SEEN>) -> Self {
-        Self {
-            addrs: input.iter().map(|a| a.into_inner()).collect(),
-        }
-    }
+    pub addrs: Vec<DetectedTag, MAX_SEEN>,
 }
 
 #[derive(Clone, Copy, Debug, Serialize, Deserialize)]
@@ -30,14 +21,40 @@ pub struct Location {
     pub latitude: f32,
     pub longitude: f32,
     pub altitude: f32,
-    pub timestamp: u64,
+    pub heading: f32,
+    #[serde(rename = "horizontalSpeed")]
+    pub horizontal_speed: f32,
+    #[serde(rename = "verticalSpeed")]
+    pub vertical_spedd: f32,
+    #[serde(rename = "timeOfFix")]
+    pub time_of_fix: u64,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct DetectedTag {
+    pub id: heapless::String<64>,
+    pub age: u16,
+    pub rssi: i8,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct GatewayUpdate {
-    pub location: Option<Location>,
     #[cfg(feature = "std")]
-    pub seen: Vec<[u8; 6]>,
+    #[serde(rename = "gatewayId")]
+    pub gateway_id: String,
     #[cfg(not(feature = "std"))]
-    pub seen: Vec<[u8; 6], MAX_SEEN>,
+    #[serde(rename = "gatewayId")]
+    pub gateway_id: heapless::String<64>,
+    pub timestamp: i64,
+
+    #[cfg(feature = "std")]
+    #[serde(rename = "detectedTags")]
+    pub detected_tags: Vec<DetectedTag>,
+    #[cfg(not(feature = "std"))]
+    #[serde(rename = "detectedTags")]
+    pub detected_tags: Vec<DetectedTag, MAX_SEEN>,
+
+    #[serde(rename = "batteryLevel")]
+    pub battery_level: Option<u8>,
+    pub location: Option<Location>,
 }
